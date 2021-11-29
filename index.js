3e26e93474a0e3a6804ce86cbd725bf8ced10551
Torsten Paul <Torsten.Paul@gmx.de>
2021-11-29 22:44:35 +0100

    Load data from index file.

---
+++ index.js
@@ -0,0 +1,4 @@
+/**/
+setDay(1, { author: 'Ulrich Bär', license: 'CC0', urlScad: 'Cone/Cone.scad', urlImg: 'Cone/Cone.png', url3mf: 'Cone/Cone.3mf' });
+setDay(2, { author: 'Ulrich Bär', license: 'CC0', urlScad: 'scad 2', urlImg: 'img 2', url3mf: '3mf 2' });
+setDay(3, { author: 'Ulrich Bär', license:'CC0', urlScad: 'scad 3', urlImg: 'img 3' });
