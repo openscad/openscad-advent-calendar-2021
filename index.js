66d657f1e2419fa7143e723fdb97a5f7d0b6e788
Ulrich Bär <93376232+UBaer21@users.noreply.github.com>
2021-11-30 15:36:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ setDay(1, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Icosahedron/Icosahed
 setDay(2, { author: 'Gürkan Şahin', license: 'CC0', urlImg: 'SphereEquation/SphereEquation.png', urlScad: 'SphereEquation/SphereEquation.scad' });
 setDay(3, { author: 'Ulrich Bär', license: 'CC0', urlImg: 'Octahedron/Octahedron.png', urlScad: 'Octahedron/Octahedron.scad' });
 setDay(4, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Tower/Tower.3mf', urlImg: 'Tower/Tower.png', urlScad: 'Tower/Tower.scad' });
-setDay(5, { author: 'Ulrich Bär', license: 'CC0', urlImg: 'Polycube/Polycube.png', urlScad: 'Polycube/Polycube.scad' });
+setDay(5, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Polycube/Polycube.3mf' ,urlImg: 'Polycube/Polycube.png', urlScad: 'Polycube/Polycube.scad' });
 setDay(6, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Star/Star.3mf', urlImg: 'Star/Star.png', urlScad: 'Star/Star.scad' });
 setDay(7, { author: 'Ulrich Bär', license: 'CC0', urlImg: 'Ornaments/Ornaments-1.png', url3mf: 'Ornaments/Ornaments.3mf', urlScad: 'Ornaments/Ornaments.scad' });
 setDay(8, { author: 'Gürkan Şahin', license: 'CC0', urlImg: 'ClimbingCircles/ClimbingCircles.png', urlScad: 'ClimbingCircles/ClimbingCircles.scad' });
 setDay(9, { author: 'Ulrich Bär', license: 'CC0', urlImg: 'Polyhedron/Polyhedron.gif', urlScad: 'Polyhedron/Polyhedron.scad' });
 setDay(10, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Honeycomb/Honeycomb.3mf', urlImg: 'Honeycomb/Honeycomb.png', urlScad: 'Honeycomb/Honeycomb.scad' });
 setDay(11, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Cone/Cone.3mf', urlImg: 'Cone/Cone.png', urlScad: 'Cone/Cone.scad' });
-setDay(12, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Dodecahedron/Dodecahedron.3mf', urlImg: 'Dodecahedron/Dodecahedron.png', urlScad: 'Dodecahedron/Dodecahedron.scad' });
+setDay(12, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Dodecahedron/Dodecahedron.3mf', url3mf2: 'Dodecahedron/DodecahedronSpheresFlat.3mf', urlImg: 'Dodecahedron/Dodecahedron.png', urlScad: 'Dodecahedron/Dodecahedron.scad' });
 setDay(13, { author: 'Ryan A. Colyer', license: 'CC0', url3mf: 'Snowballs/Snowballs.3mf', urlImg: 'Snowballs/Snowballs.png', urlScad: 'Snowballs/Snowballs.scad' });
 setDay(14, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'ColorCube/ColorCube.3mf', urlImg: 'ColorCube/ColorCube.gif', urlScad: 'ColorCube/ColorCube.scad' });
 setDay(15, { author: 'Ryan A. Colyer', license: 'CC0', url3mf: 'KeyHolder/KeyHolder.3mf', urlImg: 'KeyHolder/KeyHolder.png', urlScad: 'KeyHolder/KeyHolder.scad' });
@@ -19,7 +19,7 @@ setDay(17, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Maze/Maze.3mf', url
 setDay(18, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Ball/Ball.3mf', urlImg: 'Ball/Ball.png', urlScad: 'Ball/Ball.scad' });
 setDay(19, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Oloid/Oloid.3mf', urlImg: 'Oloid/Oloid.png', urlScad: 'Oloid/Oloid.scad' });
 setDay(20, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Tree/Tree.3mf', urlImg: 'Tree/Tree.png', urlScad: 'Tree/Tree.scad' });
-setDay(21, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Penrose/Penrose.3mf', url3mf2: 'Penrose/Penrose3D.3mf', urlImg: 'Penrose/PenroseXS.gif', urlScad: 'Penrose/Penrose.scad' });
+setDay(21, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Penrose/Penrose.3mf', url3mf2: 'Penrose/Penrose3D.3mf', urlImg: 'Penrose/Penrose.gif', urlScad: 'Penrose/Penrose.scad' });
 setDay(22, { author: 'Ulrich Bär', license: 'CC0', urlImg: 'Neuron/Neuron.png', urlScad: 'Neuron/Neuron.scad' });
 setDay(23, { author: 'Ulrich Bär', license: 'CC0', url3mf: 'Zen/Zen.3mf', urlImg: 'Zen/Zen.png', urlScad: 'Zen/Zen.scad' });
 setDay(24, { author: 'Ryan A. Colyer', license: 'CC0', url3mf: 'Decomposition/Decomposition.3mf', urlImg: 'Decomposition/Decomposition.gif', urlScad: 'Decompositon/Decomposition.scad' });
